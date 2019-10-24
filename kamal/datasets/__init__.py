6bfc9e3dcff2d657062d4a52f155e08cab699952
Ssssseason <1904130447@qq.com>
2019-10-24 20:34:59 +0800

    Update sbm

--- kamal/datasets/__init__.py
+++ kamal/datasets/__init__.py
@@ -4,7 +4,7 @@ from .voc import VOCSegmentation
 from .cub200 import CUB200
 from .fgvc_aircraft import FGVCAircraft
 from .imagenet import ImageNet
-from .nyu import NYUv2
+from .nyu import NYUv2, NYU_DEPTH_BIN, NYU_LENGTH_BIN
 from .stanford_dogs import StanfordDogs
 from .stanford_cars import StanfordCars
 from .kitti import Kitti
--- kamal/metrics/stream_metrics.py
+++ kamal/metrics/stream_metrics.py
@@ -391,3 +391,64 @@ class StreamAngleMetrics(_StreamMetrics):
         self.preds = None
         self.targets = None
         self.masks = None
+
+class CEMAPMetric():
+    def __init__(self):
+        self.targets = None
+        self.preds = None
+
+    def update(self, preds, targets):
+        # targets: -1 negative, 0 difficult, 1 positive
+        if isinstance(preds, torch.Tensor):
+            preds = preds.cpu().numpy()
+            targets = targets.cpu().numpy()
+
+        self.preds = preds if self.preds is None else np.append(self.preds, preds, axis=0)
+        self.targets = targets if self.targets is None else np.append(self.targets, targets, axis=0)
+
+    @staticmethod
+    def to_str(results):
+        string = "\n"
+        for k, v in results.items():
+            string += '{}: {}\n'.format(k, v)
+
+        return string
+
+    def get_results(self):
+        nTest = self.targets.shape[0]
+        nLabel = self.targets.shape[1]
+        eap = np.zeros(nTest)
+        for i in range(0,nTest):
+            R = np.sum(self.targets[i,:]==1)
+            for j in range(0,nLabel):            
+                if self.targets[i,j]==1:
+                    r = np.sum(self.preds[i,np.nonzero(self.targets[i,:]!=0)]>=self.preds[i,j])
+                    rb = np.sum(self.preds[i,np.nonzero(self.targets[i,:]==1)] >= self.preds[i,j])
+
+                    eap[i] = eap[i] + rb/(r*1.0)
+            eap[i] = eap[i]/R
+        # emap = np.nanmean(ap)
+
+
+        cap = np.zeros(nLabel)
+        for i in range(0,nLabel):
+            R = np.sum(self.targets[:,i]==1)
+            for j in range(0,nTest):
+                if self.targets[j,i]==1:
+                    r = np.sum(self.preds[np.nonzero(self.targets[:,i]!=0),i] >= self.preds[j,i])
+                    rb = np.sum(self.preds[np.nonzero(self.targets[:,i]==1),i] >= self.preds[j,i])
+                    cap[i] = cap[i] + rb/(r*1.0)
+            cap[i] = cap[i]/R
+        # cmap = np.nanmean(ap)
+
+        return {
+            'eap': eap,
+            'emap': np.nanmean(eap),
+            'cap': cap,
+            'cmap': np.nanmean(cap),
+        }
+
+    def reset(self):
+        self.preds = None
+        self.targets = None
+        
