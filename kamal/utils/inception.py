bb030c48751d110cf517d9fe35111433974b07da
lingyaoluu <lingyaoluu@163.com>
2023-06-28 09:02:20 +0000

    add cmi

--- kamal/utils/inception.py
+++ kamal/utils/inception.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 import torchvision
 
 try:
-    from torch.hub import load_state_dict_from_url
+    from torchvision.models.utils import load_state_dict_from_url
 except ImportError:
     from torch.utils.model_zoo import load_url as load_state_dict_from_url
 
